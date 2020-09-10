bbf407380d36379e3dfde7982f55ede3b82acb4b
roydenwa <royden-wagner@outlook.com>
2020-09-10 16:27:54 +0200

    sleep between steering

--- NO_ML/autonomous_driving.py
+++ NO_ML/autonomous_driving.py
@@ -1,15 +1,20 @@
+import time
+
 from driving_functions import steering, motor_ctrl
 
 
 if "__name__" == "__main__":
     # max right:
     steering(25)
+    time.sleep(3)
 
     # max left:
     steering(-25)
+    time.sleep(3)
 
     # "medium" right:
     steering(12.5)
+    time.sleep(3)
 
     # "medium" left:
     steering(-12.5)
