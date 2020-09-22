48c99cbb4ceff3258d9a896233e9e18dbe8268e0
roydenwa <royden-wagner@outlook.com>
2020-09-22 17:22:26 +0200

    lane_detection on video

--- NO_ML/lane_detection.py
+++ NO_ML/lane_detection.py
@@ -4,10 +4,24 @@ import numpy as np
 from dataclasses import dataclass, field
 
 
+def split_lines(array):
+    ''' helper func to distinguish lines marking left and right lanes '''
+    left_lines, right_lines = [], []
+
+    for _, item in enumerate(array):
+        if ((item[0, 0] == 0) and (item[0, 1] > item[0, 3])):
+            left_lines.append(item)
+        else:
+            right_lines.append(item)
+
+    return np.array(left_lines), np.array(right_lines)
+
+
 def lane_detection(func):
     ''' decorator to detect lanes in video frame '''
     def func_wrapper(*args, **kwargs):
         frame = func(*args, **kwargs)
+        lanes = []
 
         # TODO: detect ROI
         white = np.ones((288, 352, 1), dtype=np.uint8) * 255
@@ -24,11 +38,22 @@ def lane_detection(func):
         # TODO: Hough line transformation
         lines = cv2.HoughLinesP(cv2.bitwise_not(roi_frame), 1, np.pi/180, 30, maxLineGap=200)
 
-        for line in lines:
-            x1, y1, x2, y2 = line[0]
-            cv2.line(frame, (x1, y1), (x2, y2), (255, 0, 0), 3)
+        # get lanes
+        left_lines, right_lines = split_lines(lines)
 
-        return roi_frame, frame
+        if left_lines.size != 0:
+            left_lane = np.mean(left_lines, axis=0, dtype=np.int32)
+            x1, y1, x2, y2 = left_lane[0]
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 0), 6)
+            lanes.append(left_lane)
+
+        if right_lines.size != 0:
+            right_lane = np.mean(right_lines, axis=0, dtype=np.int32)
+            x1, y1, x2, y2 = right_lane[0]
+            cv2.line(frame, (x1, y1), (x2, y2), (0, 0, 255), 6)
+            lanes.append(right_lane)
+
+        return frame, lanes
 
     return func_wrapper
 
@@ -60,9 +85,8 @@ if __name__ == "__main__":
     video = Video(0, 352, 288)
 
     while True:
-        frame, stencil = video.get_frame()
+        frame, lanes = video.get_frame()
         cv2.imshow("frame", frame)
-        cv2.imshow("stencil", stencil)
 
         if cv2.waitKey(1) & 0xFF == ord('q'):
             break
