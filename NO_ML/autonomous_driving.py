ea9611de775a14fbf68f107616e7acdef7b6cb4c
roydenwa <royden-wagner@outlook.com>
2020-09-26 11:55:37 +0200

    cv2 doesn't work with f-strings

--- NO_ML/autonomous_driving.py
+++ NO_ML/autonomous_driving.py
@@ -51,7 +51,7 @@ def main():
 
         x1, y1, x2, y2 = direction[0]
         cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 255), 3)
-        cv2.putText(frame, f"steeringangle: {steeringangle}", (0, 0),
+        cv2.putText(frame, "steeringangle: " + str(steeringangle), (0, 0),
                     cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
 
         cv2.imshow("frame", frame)
