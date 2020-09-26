c89a7c35f2946badf4a43a10d6252d7ebcab4e40
roydenwa <royden-wagner@outlook.com>
2020-09-26 11:59:48 +0200

    lowered fonstscale

--- NO_ML/autonomous_driving.py
+++ NO_ML/autonomous_driving.py
@@ -51,8 +51,8 @@ def main():
 
         x1, y1, x2, y2 = direction[0]
         cv2.line(frame, (x1, y1), (x2, y2), (0, 255, 255), 3)
-        cv2.putText(frame, "steeringangle: " + str(steeringangle), (0, 0),
-                    cv2.FONT_HERSHEY_SIMPLEX, 2, 255)
+        cv2.putText(frame, f"steeringangle: {steeringangle}", (20, 20),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.5, 255)
 
         cv2.imshow("frame", frame)
         cv2.imshow("frame w/ lines", frame_lines)
