09c7f6f89bbff495d8410af6fbdcdc6f0d005746
Denis Münch <denis.muench@stud.uni-heidelberg.de>
2020-10-02 11:51:37 +0200

    Remove crop.

--- ML/custom_camera.py
+++ ML/custom_camera.py
@@ -2,9 +2,6 @@ from donkeycar.parts.dgym import DonkeyGymEnv
 import numpy as np
 import cv2
 
-def crop(frame):
-    return frame[50:,:,:]
-
 class CustomDonkeyGymEnv(DonkeyGymEnv):
     def __init__(self, sim_path, host="127.0.0.1", port=9091, headless=0, env_name="donkey-generated-track-v0", sync="asynchronous", conf={}, delay=0):
         super().__init__(sim_path, host, port, headless, env_name, sync, conf, delay)
@@ -18,4 +15,3 @@ class CustomDonkeyGymEnv(DonkeyGymEnv):
     def update(self):        
         while self.running:
             self.frame, _, _, self.info = self.env.step(self.action)
-            #self.frame = self.frame[50:,:,:]
